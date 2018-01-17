94a3f3c4e6b5b5ba8f4cffaaa41854b022a2328e
Ildikó Mag Mokus Horváth <horvath.ildiko.mm@gmail.com>
2018-01-17 13:44:26 +0100

    cat clicker duo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 var cnt=0;
-$('#catImg').click(function(){
+var cats =
+  [{name:"meeow1", img:"img/Peterbold3.jpg" },
+  {name:"meeow2",img:"img/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"}]
+cats.map(function(cat){
+  $("#catlist").append("<figure><img class=\"catImg\" src="+cat.img+" alt=\"Cat\"> <figurecaption>"+ cat.name +"</figurecaption> </figure>");
+});
+
+$('.catImg').click(function(){
   cnt++;
   $(".numOfClick").text(cnt);
 });
+//<figure><img class="catImg" src=cat.img alt="Cat"> <figurecaption> cat.name </figurecaption> </figure>
